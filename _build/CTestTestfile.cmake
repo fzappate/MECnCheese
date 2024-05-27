fc73e84daa4d69a330c506bfc91e9f80364bc7ef
fzappate@purdue.edu <53621597+fzappate@users.noreply.github.com>
2024-05-27 14:37:31 -0500

    Add build to gitignore

--- _build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Software/MECAndCheese
-# Build directory: C:/Software/MECAndCheese/_build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
