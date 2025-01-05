db92ea157200392c565fb724f3f2ad6f57fb24a2
Federico Zappaterra <53621597+fzappate@users.noreply.github.com>
2025-01-04 19:58:27 -0600

    Added Diff and NonDiffEquations in .h and .cpp

--- src/equation.cpp
+++ src/equation.cpp
@@ -3,3 +3,7 @@
 #include "./equation.h"
 
 Equation::Equation(std::string name) : name(name){};
+
+DiffEquation::DiffEquation(std::string name) : Equation(name){};
+
+NonDiffEquation::NonDiffEquation(std::string name) : Equation(name){};
